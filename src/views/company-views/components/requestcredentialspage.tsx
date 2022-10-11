182decbba37ff989823537ba618c0895aa44a34b
deepakagg <deepak.aggarwal9@gmail.com>
2022-10-11 09:53:39 +0530

    Design and workflow changes

--- src/views/company-views/components/requestcredentialspage.tsx
+++ src/views/company-views/components/requestcredentialspage.tsx
@@ -1,4 +1,12 @@
-import { Drawer, Button, Row } from 'antd';
+import {
+    Card,
+    Col,
+    Row,
+    Modal,
+    CardImg,
+    ModalFooter,
+    Button,
+} from "reactstrap";
 import styled from "styled-components";
 
 const StyledActionButton = styled.div`
@@ -18,8 +26,22 @@ const StyledLink = styled.a`
     font-weght: bold;
 `;
 
+const headerStyle = {
+    fontSize: "16px",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+};
+
+const btnSz = {
+    height: "1.8rem",
+    width: "10rem",
+    padding: 0,
+    fontSize: "12px",
+};
+
 export const RequestCredentialsPage = (props: { onClose: any; open: boolean; }) => {
-    const footerActionButtons = () => {
+    /* const footerActionButtons = () => {
         return (
             <div>
                 <Row>
@@ -34,15 +56,44 @@ export const RequestCredentialsPage = (props: { onClose: any; open: boolean; }) 
                 </Row>
             </div>
         );
-    }
+    } */
 
     return (
-        <Drawer title="REQUEST CREDENTIALS" placement="right" onClose={props.onClose} open={props.open} footer={footerActionButtons()}>
-            <p>
-                You are about to request credentials from Bolagsverket, Sweden. Confirm the details below and click submit the request to issue credentials.
-            </p>
-            <p>Once submitted, the requested credentials will be issued to the <StyledLink href="/company/">configured wallet</StyledLink>.</p>
-        </Drawer>
+        <Modal backdrop="static" unmountOnClose={true} isOpen={props.open}>
+            <div className="modal-content">
+                <div className="modal-header" style={headerStyle}>
+
+                    <h3 style={{ marginBottom: "0px", fontWeight: "bold" }}>REQUEST CREDENTIALS</h3>
+
+                    <button type="button" className="close" aria-label="Close" onClick={props.onClose}>
+                        <span aria-hidden="true">×</span>
+                    </button>
+
+                </div>
+                <div className="modal-body-without-top-padding" style={{ paddingTop: "0" }}>
+                    {/* <Drawer title="REQUEST CREDENTIALS" placement="right" onClose={props.onClose} open={props.open} footer={footerActionButtons()}> */}
+                    <p>
+                        You are about to request credentials from Bolagsverket, Sweden. Confirm the details below and click submit the request to issue credentials.
+                    </p>
+                    <p>
+                        Requested credential: Certificate of registration and register extract
+                    </p>
+                    <p>Once submitted, the requested credentials will be issued to the <StyledLink href="/company/">configured wallet</StyledLink>.</p>
+                    {/* </Drawer> */}
+                </div>
+                <ModalFooter className="modal-footer">
+
+                    <button style={btnSz} className="btn btn-default" onClick={props.onClose}>Cancel</button>
+
+                    <button
+                        style={btnSz}
+                        className="btn btn-default"
+                        onClick={() => { }}
+                    >Submit</button>
+
+                </ModalFooter>
+            </div>
+        </Modal >
     );
 };
 
--- src/views/company-views/components/walletdetailspage.tsx
+++ src/views/company-views/components/walletdetailspage.tsx
@@ -1,39 +1,155 @@
-import { Alert, Button, Drawer } from 'antd';
+import {
+    Col,
+    Row,
+    Modal,
+    CardImg,
+    ModalFooter,
+    Button,
+} from "reactstrap";
+import { Tooltip, Radio, RadioGroup, FormControl, FormControlLabel, FormLabel } from "@material-ui/core";
+import { Card } from "antd";
 import styled from "styled-components";
+import React, { useState } from 'react';
 
 const StyledActionButton = styled.div`
     margin-bottom: 10px;
     width: 100%;
 `;
 
-const StyledAlert = styled(Alert)`
+const StyledAlert = styled(Card)`
     background-color: #ddd;
     border-color: #505050;
+    font-size: 12px;
+    font-weight: 400;
+    margin-top: 0px;
 `;
 
-export const WalletDetailsPage = (props: { onClose: any; open: boolean; }) => {
-    const footerActionButtons = () => {
-        return (
-            <div>
-                <StyledActionButton>
-                    <Button block size={"middle"} onClick={() => { }}>Cancel</Button>
-                </StyledActionButton>
-            </div>
-        );
-    }
+const StyledWalletDiv = styled.div`
+    font-size: 12px;
+    font-weight: 600;
+`;
+
+const StyledAnchor = styled.a`
+    font-size: 12px;
+    font-weight: 600;
+    color: blue !important;
+`;
+
+const headerStyle = {
+    fontSize: "16px",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+};
+
+const btnSz = {
+    height: "1.8rem",
+    width: "10rem",
+    padding: 0,
+    fontSize: "12px",
+};
+
+export const WalletDetailsPage = (props: { onClose: any; open: boolean; walletData: any }) => {
+    // const footerActionButtons = () => {
+    //     return (
+    //         <div>
+    //             <StyledActionButton>
+    //                 <Button block size={"middle"} onClick={() => { }}>Cancel</Button>
+    //             </StyledActionButton>
+    //         </div>
+    //     );
+    // }
+
+    const { walletData } = props;
+    const [selectedValue, setSelectedValue] = useState('a');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedValue(event.target.value);
+    };
+
+    console.log({ walletData })
 
     return (
-        <Drawer title="YOUR WALLET" placement="right" onClose={props.onClose} open={props.open} footer={footerActionButtons()}>
-            <p>
-                This provides information on your wallet connected to Bolagsverket, Sweden.
-            </p>
-            <p>
-                <StyledAlert
-                    message={'Wallet Details'}
-                    type="info"
-                />
-            </p>
-        </Drawer>
+        <Modal backdrop="static" unmountOnClose={true} isOpen={props.open}>
+            <div className="modal-content">
+                <div className="modal-header" style={headerStyle}>
+
+                    <h3 style={{ marginBottom: "0px", fontWeight: "bold" }}>MY WALLET</h3>
+
+                    <button type="button" className="close" aria-label="Close" onClick={props.onClose}>
+                        <span aria-hidden="true">×</span>
+                    </button>
+
+                </div>
+                <div className="modal-body-without-top-padding" style={{ paddingTop: "0" }}>
+                    <div>
+                        This provides information on your wallet connected to Bolagsverket, Sweden.
+                    </div>
+                    <div>
+                        <FormControlLabel
+                            value="Data Source"
+                            control={<Radio
+                                checked={selectedValue === 'a'}
+                                onChange={handleChange}
+                                value="a"
+                                name="radio-buttons"
+                                inputProps={{ 'aria-label': 'A' }}
+                                color="default"
+
+                            />}
+                            label={<span style={{ fontSize: '14px', fontFamily: 'Helvetica, Helvetica neue' }}>Default Wallet (Provided by Bolagsverket)</span>}
+                        />
+                    </div>
+                    {selectedValue === 'a' ? <div>
+                        <StyledAlert>
+                            <h1 style={{ 'fontSize': '20px', 'fontWeight': 600 }}>
+                                Wallet Details
+                            </h1>
+                            <StyledWalletDiv>
+                                Agent service endpoint
+                            </StyledWalletDiv>
+                            <StyledWalletDiv>
+                                <StyledAnchor>{walletData.serviceEndpoint}</StyledAnchor>
+                            </StyledWalletDiv>
+                            <StyledWalletDiv>Agent label: {walletData.label}</StyledWalletDiv>
+                            <StyledWalletDiv>DID: {(walletData['@type'] || '').split(';')[0]}</StyledWalletDiv>
+                        </StyledAlert>
+                    </div> : null}
+                    <div>
+                        <FormControlLabel
+                            value="Data Source"
+                            control={<Radio
+                                checked={selectedValue === 'b'}
+                                onChange={handleChange}
+                                value="b"
+                                name="radio-buttons"
+                                inputProps={{ 'aria-label': 'B' }}
+                                color="default"
+                            />}
+                            label={<span style={{ fontSize: '14px', fontFamily: 'Helvetica, Helvetica neue' }}>Own Wallet (Configure)</span>}
+                        />
+                    </div>
+                    {selectedValue === 'b' ? <div>
+                        <StyledAlert>
+                            <h1 style={{ 'fontSize': '20px', 'fontWeight': 600 }}>
+                                Wallet Details
+                            </h1>
+                            <StyledWalletDiv>
+                                Agent service endpoint
+                            </StyledWalletDiv>
+                            <StyledWalletDiv>
+                                <StyledAnchor>{walletData.serviceEndpoint}</StyledAnchor>
+                            </StyledWalletDiv>
+                            <StyledWalletDiv>Agent label: {walletData.label}</StyledWalletDiv>
+                            <StyledWalletDiv>DID: {(walletData['@type'] || '').split(';')[0]}</StyledWalletDiv>
+                        </StyledAlert>
+                    </div> : null}
+                </div>
+                <ModalFooter className="modal-footer">
+                    <button style={btnSz} className="btn btn-default" onClick={props.onClose}>Cancel</button>
+                </ModalFooter>
+            </div>
+        </Modal >
     );
 };
 
