2f9d66ecb1e1b084b0c977286c0f5da10f3f4a03
deepakagg <deepak.aggarwal9@gmail.com>
2022-10-13 13:04:56 +0530

    Design fixes

--- src/views/company-views/components/landingpage.tsx
+++ src/views/company-views/components/landingpage.tsx
@@ -1,5 +1,6 @@
-import { Col, Row, Card, Input, Avatar, Button, Layout, Divider, Space } from 'antd';
-import { UserOutlined, HomeFilled, WalletOutlined, BellOutlined, SettingOutlined } from '@ant-design/icons';
+import { Col, Row, Card, Input, Avatar, Button, Layout, Divider, Space, Grid } from 'antd';
+import { UserOutlined, HomeFilled, WalletOutlined, BellOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Popover, PopoverBody, ListGroup, ListGroupItem, Card as ReactStrapCard, CardTitle, CardSubtitle, CardText, CardBody } from 'reactstrap';
 import React, { useState, useEffect } from 'react';
 import RequestCredentialsPage from "./requestcredentialspage";
 import WalletDetailsPage from './walletdetailspage';
@@ -13,6 +14,7 @@ import axiosService from 'services/axiosService';
 import companyService from 'services/companyService';
 import headerLogo from '../../../assets/img/reactlogo.png';
 import walletIcon from '../../../assets/img/icons/wallet.png';
+const { useBreakpoint } = Grid;
 
 
 
@@ -53,7 +55,7 @@ const StyledText = styled.h3`
 `;
 
 const StyledTextH1 = styled.h1`
-    font-size: 28px;
+    font-size: 18px;
     margin-bottom: 20px;
 `;
 
@@ -81,11 +83,16 @@ const StyledCompanyHeader = styled.h1`
     margin-right: 10px;
 `;
 
+const StyledCompanyHeaderMobile = styled.h1`
+    font-size: 30px;
+    text-align: right;
+    margin-right: 10px;
+`;
+
 const StyledTextH2 = styled.h2`
     font-size: 30px;
     font-weight: 400;
     text-align: left;
-    padding-left: 20px;
 `;
 
 const StyledButton = styled(Button)`
@@ -106,7 +113,7 @@ const StyledButton = styled(Button)`
 `; */
 
 const StyledFooterLink = styled.a`
-    font-size: 12px;
+    font-size: 10px;
     color: white;
 `;
 
@@ -121,6 +128,7 @@ export const LandingPage = () => {
     const [certificates, setCertificates] = useState<string[]>([]);
     const [lastCertificateData, setLastCertificateData] = useState<any>({});
     const history = useHistory();
+    const [openAvatar, setOpenAvatar] = useState(false);
 
     /* const checkPermission = async () => {
         try {
@@ -247,38 +255,58 @@ export const LandingPage = () => {
         }
     }, [certificates.join(',')])
 
+    const screens = useBreakpoint();
+    const isMobile = screens['xs'] ? true : false;
+
     return (
-        <div>
-            <div>
+        <div style={{ backgroundColor: "#F5F5F5", height: "100%" }}>
+            <div style={{ maxWidth: '1080px', margin: 'auto' }}>
                 <Row gutter={16}>
                     <Col span={24}>
                         <StyledHeaderCardDefault style={{
-                            margin: "20px",
+                            marginTop: "20px",
+                            marginBottom: "20px",
                             overflow: "hidden",
                             borderWidth: "thin",
-                            borderColor: "black",
-                            backgroundColor: "#F5F5F5"
+                            borderColor: "black"
                         }}>
-                            <Row style={{ 'alignItems': 'center' }} gutter={8}>
-                                <Col span={1}>
+                            <Row style={{ 'alignItems': 'center' }}>
+                                <Col lg={1} md={1} sm={1} xs={1}>
                                     <img src={headerLogo} style={{ width: "100px", marginLeft: "-10px" }} alt="header" />
                                 </Col>
-                                <Col span={7}>
-                                    <StyledCompanyHeader>MyCompany Portal</StyledCompanyHeader>
+                                <Col lg={10} md={20} sm={20} xs={20}>
+                                    {!isMobile ? <StyledCompanyHeader>MyCompany Portal</StyledCompanyHeader> : <StyledCompanyHeaderMobile>MyCompany Portal</StyledCompanyHeaderMobile>}
                                 </Col>
-                                <Col span={4}>
-                                    <Search
-                                        placeholder="input search text"
-                                        allowClear
-                                        size="large"
-                                        onSearch={onSearch}
-                                    />
+                                <Col lg={2} md={3} sm={3} xs={3} offset={!isMobile ? 5 : 0}>
+                                    <div>
+                                        <div id="UserAvatar">
+                                            <Avatar size={!isMobile ? 110 : 50} icon={<UserOutlined />} onClick={() => { setOpenAvatar(true); }} />
+                                        </div>
+                                        {isMobile ? <Popover
+                                            placement="bottom-end"
+                                            isOpen={openAvatar}
+                                            toggle={() => { setOpenAvatar(!openAvatar); }}
+                                            target="UserAvatar"
+                                            className="p-0 border-0"
+                                            style={{ minWidth: 250 }}>
+                                            <PopoverBody className="p-0">
+                                                <ReactStrapCard inverse className={"bg-gradient-theme border-primary"}>
+                                                    <CardBody className="d-flex justify-content-center align-items-center flex-column text-center">
+                                                        <Avatar size={70} icon={<UserOutlined />} />
+                                                        <CardTitle>{'Johan Eriksson'}</CardTitle>
+                                                        <CardSubtitle><small>{'Bygg AB'}</small></CardSubtitle>
+                                                    </CardBody>
+                                                    <ListGroup flush>
+                                                        <ListGroupItem tag="button" action className="border-0 border-light bg-light" onClick={onLogoutClick}>
+                                                            <LogoutOutlined /> Signout
+                                                        </ListGroupItem>
+                                                    </ListGroup>
+                                                </ReactStrapCard>
+                                            </PopoverBody>
+                                        </Popover> : null}
+                                    </div>
                                 </Col>
-                                <Col span={6}></Col>
-                                <Col span={2}>
-                                    <Avatar size={110} icon={<UserOutlined />} onClick={() => { }} />
-                                </Col>
-                                <Col span={4} style={{ textAlign: 'right' }}>
+                                {!isMobile ? <Col span={6} style={{ textAlign: 'right' }}>
                                     <Row>
                                         <Col span={24}>
                                             <StyledTextName>Johan Eriksson</StyledTextName>
@@ -292,41 +320,19 @@ export const LandingPage = () => {
                                     <Row>
                                         <Col span={24}><StyledTextLogout onClick={onLogoutClick}>Logout</StyledTextLogout></Col>
                                     </Row>
-                                </Col>
+                                </Col> : null}
                             </Row>
                         </StyledHeaderCardDefault>
                     </Col>
                 </Row>
                 <Row>
-                    <Col span={24}>
-                        <StyledBreadCrumbCardDefault style={{
-                            margin: " 0 20px 20px 20px ",
-                            overflow: "hidden",
-                            backgroundColor: "#F5F5F5",
-                            borderWidth: "0px",
-                        }}>
-                            <Row>
-                                <Col span={1}>
-                                    <HomeFilled style={{ fontSize: '25px' }} />
-                                </Col>
-                                <Col span={1}>
-                                    <StyledText>Home</StyledText>
-                                </Col>
-                            </Row>
-                        </StyledBreadCrumbCardDefault>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={6}>
+                    <Col span={24} style={{ marginLeft: "10px" }}>
                         <StyledTextH2>Welcome Johan Eriksson!</StyledTextH2>
                     </Col>
                 </Row>
-
-                <Row style={{ 'paddingTop': '20px' }}>
-                    <Col span={12}>
+                <Row style={{ 'paddingTop': '20px' }} gutter={[16, 16]}>
+                    <Col lg={12} md={24} sm={24} xs={24}>
                         <StyledCardDefault style={{
-                            marginLeft: "20px",
-                            marginRight: "10px",
                             overflow: "hidden",
                             borderWidth: "thin",
                             borderColor: "#ddd",
@@ -334,9 +340,7 @@ export const LandingPage = () => {
                             minHeight: "225px"
                         }}>
                             <Row style={{ 'textAlign': 'left' }}>
-                                <Col span={24}>
-                                    <StyledTextH1>Certificate of registration and register extract</StyledTextH1>
-                                </Col>
+                                <StyledTextH1>Certificate of registration and register extract</StyledTextH1>
                             </Row>
                             <Row>
                                 <StyledButton type="primary" block size={"large"}
@@ -346,18 +350,14 @@ export const LandingPage = () => {
                                 </StyledButton>
                             </Row>
                             <Row style={{ 'textAlign': 'center' }}>
-                                <Col span={24}>
-                                    <div style={{ color: "green", fontSize: "12px" }}>
-                                        Your request is being processed. Once processed, your configured wallet will be notified.
-                                    </div>
-                                </Col>
+                                <div style={{ color: "green", fontSize: "9px" }}>
+                                    Your request is being processed. Once processed, your configured wallet will be notified.
+                                </div>
                             </Row>
                         </StyledCardDefault>
                     </Col>
-                    <Col span={12}>
+                    <Col lg={12} md={24} sm={24} xs={24}>
                         <StyledCardDefault style={{
-                            marginRight: "20px",
-                            marginLeft: "10px",
                             overflow: "hidden",
                             borderWidth: "thin",
                             borderColor: "#ddd",
@@ -371,7 +371,6 @@ export const LandingPage = () => {
                                     <BellOutlined style={{ fontSize: '35px' }} onClick={showViewCredentialsDrawer} />
                                     <SettingOutlined style={{ fontSize: '35px' }} onClick={showWalletConfigurationsDrawer} />
                                 </Space>
-
                             </Row>
                             <Row>
                                 <Col>
@@ -379,17 +378,16 @@ export const LandingPage = () => {
                                 </Col>
                             </Row>
                             <Row>
-                                <Col span={8}></Col>
-                                <Col span={1}>
-                                    <img src={walletIcon} alt={'wallet'}/>
+                                <Col span={1} offset={7}>
+                                    <img src={walletIcon} alt={'wallet'} />
                                 </Col>
-                                <Col span={8} onClick={showWalletDetailsDrawer}>
-                                    <StyledTextH1 style={{ marginTop: '8px', marginLeft: '30px' }}>My Wallet</StyledTextH1>
+                                <Col span={12} offset={1} onClick={showWalletDetailsDrawer}>
+                                    <StyledTextH1 style={{ marginTop: '16px', marginLeft: '30px' }}>My Wallet</StyledTextH1>
                                 </Col>
                             </Row>
                             <Row style={{ 'textAlign': 'center' }}>
                                 <Col span={24}>
-                                    <div style={{ color: "green", fontSize: "12px" }}>
+                                    <div style={{ color: "green", fontSize: "9px" }}>
                                         New certificate in your wallet. Click to view.
                                     </div>
                                 </Col>
@@ -415,14 +413,14 @@ export const LandingPage = () => {
                     </Row>
                     <Row>
                         <Col span={24}>
-                            <div style={{ fontWeight: "bold", color: 'white', fontSize: '12px' }}>
+                            <div style={{ fontWeight: "bold", color: 'white', fontSize: '10px' }}>
                                 Bolagsverket SE -851 81 Sundsvall Sweden
                             </div>
                         </Col>
                     </Row>
                     <Row>
                         <Col span={24}>
-                            <div style={{ color: 'white', fontSize: '12px' }}>
+                            <div style={{ color: 'white', fontSize: '10px' }}>
                                 © Bolagsverket, Sweden
                             </div>
                         </Col>
