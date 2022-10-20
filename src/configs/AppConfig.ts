8b481cb1128e39ae7db6232b6c5d8513976e953b
deepakagg <deepak.aggarwal9@gmail.com>
2022-10-20 12:08:47 +0530

    Zoom fix and moved login component to path /

--- src/configs/AppConfig.ts
+++ src/configs/AppConfig.ts
@@ -1,5 +1,5 @@
 export const HOME_PREFIX_PATH = '/';
-export const AUTH_PREFIX_PATH = '/auth';
+export const AUTH_PREFIX_PATH = '/';
 export const COMPANY_PREFIX_PATH = '/company';
 export const PROCUREMENT_BUYER_PREFIX_PATH = '/procurement-buyer';
 export const PROCUREMENT_SUPPLIER_PREFIX_PATH = '/procurement-supplier';
